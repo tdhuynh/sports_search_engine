8617cc16182fbc29a6144e882e235a210ac48ed4
tdhuynh <tommyhuynh93@gmail.com>
2016-09-29 23:02:28 -0400

    migration working, and fixed missing items in csv

--- migration.py
+++ migration.py
@@ -12,23 +12,24 @@ CREATE TABLE ufc_stats_table (
     id serial PRIMARY KEY,
     first_name VARCHAR(20),
     last_name VARCHAR(20),
-    strikes NUMERIC(4),
-    strike_accuracy NUMERIC(4,4),
-    takedowns NUMERIC(2),
-    takedown_accuracy NUMERIC(4,4),
-    knockdowns NUMERIC(1),
-    passes NUMERIC(2),
-    reversals NUMERIC(1),
-    submissions NUMERIC(1)
+    fights SMALLINT,
+    strikes SMALLINT,
+    strike_accuracy NUMERIC(3,3),
+    takedowns SMALLINT,
+    takedown_accuracy NUMERIC(3,3),
+    knockdowns SMALLINT,
+    passes SMALLINT,
+    reversals SMALLINT,
+    submissions SMALLINT
 );
 """
 cursor.execute(create_table_command)
 
 with open('stats.csv') as file:
     contents = list(csv.reader(file))
-    for row in contents:
-        cursor.execute("INSERT INTO ufc_stats_table VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-        (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],))
+for row in contents:
+    cursor.execute("INSERT INTO ufc_stats_table VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+    (row[:13]))
 
 
 # cursor.execute("INSERT INTO ufc_stats_table VALUES (1, 'NATE DIAZ', '1926', '.538', '17', '.262', '3', '15', '6', '9')")
