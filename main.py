1ae52bf28f0ebd458b3f131c560517ee6bcb2924
tdhuynh <tommyhuynh93@gmail.com>
2016-10-08 13:42:22 -0400

    fixed some errors and mistakes

--- main.py
+++ main.py
@@ -6,98 +6,231 @@ import sys
 connection = psycopg2.connect("dbname=ufc_allweights_stats user=ufc_allweights_stats")
 cursor = connection.cursor()
 
+
 def first_name():
-    if look_up == 'fn':
-        first_name = input("First name: ").upper()
-        cursor.execute("SELECT * from ufc_stats_table WHERE first_name = %s;", (first_name,))
-        results = cursor.fetchall()
-        print(results)
+    first_name = input("First name: ").upper()
+    cursor.execute("SELECT * from ufc_stats_table WHERE first_name = %s;", (first_name,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
+
 
 def last_name():
-    if look_up == 'ln':
-        last_name = input("Last name: ").upper()
-        cursor.execute("SELECT * from ufc_stats_table WHERE last_name = %s;", (last_name,))
-        results = cursor.fetchall()
-        print(results)
+    last_name = input("Last name: ").upper()
+    cursor.execute("SELECT * from ufc_stats_table WHERE last_name = %s;", (last_name,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def fights():
-    if look_up == 'f':
-        fights = input("Fights: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE fights = %s;", (fights,))
-        results = cursor.fetchall()
-        print(results)
+    fights = input("Fights: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE fights = %s;", (fights,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def strikes():
-    if look_up == 's':
-        strikes = input("Strikes: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE strikes = %s;", (strikes,))
-        results = cursor.fetchall()
-        print(results)
+    strikes = input("Strikes: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE strikes = %s;", (strikes,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def strike_accuracy():
-    if look_up == 'sa':
-        strike_accuracy = input("Strike accuracy: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE strike_accuracy = %s;", (strike_accuracy,))
-        results = cursor.fetchall()
-        print(results)
+    strike_accuracy = input("Strike accuracy: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE strike_accuracy = %s;", (strike_accuracy,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def takedowns():
-    if look_up == 't':
-        takedowns = input("Takedowns: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE takedowns = %s;", (takedowns,))
-        results = cursor.fetchall()
-        print(results)
+    takedowns = input("Takedowns: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE takedowns = %s;", (takedowns,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def takedown_accuracy():
-    if look_up == 'ta':
-        takedown_accuracy = input("Takedown accuracy: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE takedown_accuracy = %s;", (takedown_accuracy,))
-        results = cursor.fetchall()
-        print(results)
+    takedown_accuracy = input("Takedown accuracy: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE takedown_accuracy = %s;", (takedown_accuracy,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def knockdowns():
-    if look_up == 'k':
-        knockdowns = input("Knockdowns: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE knockdowns = %s;", (knockdowns,))
-        results = cursor.fetchall()
-        print(results)
+    knockdowns = input("Knockdowns: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE knockdowns = %s;", (knockdowns,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def passes():
-    if look_up == 'p':
-        passes = input("Passes: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE passes = %s;", (passes,))
-        results = cursor.fetchall()
-        print(results)
+    passes = input("Passes: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE passes = %s;", (passes,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def reversals():
-    if look_up == 'r':
-        reversals = input("Reversals: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE reversals = %s;", (reversals,))
-        results = cursor.fetchall()
-        print(results)
+    reversals = input("Reversals: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE reversals = %s;", (reversals,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def submissions():
-    if look_up == 'su':
-        submissions = input("Submissions: ")
-        cursor.execute("SELECT * from ufc_stats_table WHERE submissions = %s;", (submissions,))
-        results = cursor.fetchall()
-        print(results)
+    submissions = input("Submissions: ")
+    cursor.execute("SELECT * from ufc_stats_table WHERE submissions = %s;", (submissions,))
+    results = cursor.fetchall()
+    for item in results:
+        print("""
+First name: {}
+Last name: {}
+Fights: {}
+Strikes: {}
+Strike accuracy: {}
+Takedown: {}
+Takedown accuracy: {}
+Knockdowns: {}
+Passes: {}
+Reversals: {}
+Submissions: {}
+""".format(item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9],item[10],item[11]))
 
 def help_initials():
-    if look_up == 'h':
-        print("""
-         Type the letter(s) in the bracket to search by that criteria:
-         [fn] first name
-         [ln] last name
-         [f] fights
-         [s] strikes
-         [sa] strike accuracy
-         [t] takedowns
-         [ta] takedown accuracy
-         [k] knockdowns
-         [p] passes
-         [r] reversals
-         [su] submissions
+    print("""
+ Type the [letter(s)] in the bracket to search by that criteria:
+ [fn] first name
+ [ln] last name
+ [f] fights
+ [s] strikes
+ [sa] strike accuracy
+ [t] takedowns
+ [ta] takedown accuracy
+ [k] knockdowns
+ [p] passes
+ [r] reversals
+ [su] submissions
          """)
 
 def create_new_data():
@@ -125,27 +258,37 @@ def create_new_data():
     (new_id,new_fn,new_ln,new_fights,new_strikes,new_strike_acc,new_takedowns,new_takedown_acc,new_knockdowns,new_passes,new_reversals,new_submissions))
     connection.commit()
 
-def stat_categories():
-    first_name()
-    last_name()
-    fights()
-    strikes()
-    strike_accuracy()
-    takedowns()
-    takedown_accuracy()
-    knockdowns()
-    passes()
-    reversals()
-    submissions()
-    help_initials()
-
 print("Welcome to a UFC All Weights Database! Here, you can look up stats of different fighters.\n")
 
-look_up_list = ['fn', 'ln', 'f', 's', 'sa', 't', 'ta', 'k', 'p', 'r', 'su', 'h']
 while True:
-    look_up = input("What would you like to do? Type 'h' for a list of commands, 'c' to create new data row, or 'q' to quit.\n>>> ").lower()
-    if look_up in look_up_list:
-        stat_categories()
+    look_up = input("""
+Which criteria would you like to look up?
+Type 'h' for a list of criteria, 'c' to create new data row, or 'q' to quit.
+>>> """).lower()
+    if look_up == 'h':
+        help_initials()
+    elif look_up == 'fn':
+        first_name()
+    elif look_up == 'ln':
+        last_name()
+    elif look_up == 'f':
+        fights()
+    elif look_up == 's':
+        strikes()
+    elif look_up == 'sa':
+        strike_accuracy()
+    elif look_up == 't':
+        takedowns()
+    elif look_up == 'ta':
+        takedown_accuracy()
+    elif look_up == 'k':
+        knockdowns()
+    elif look_up == 'p':
+        passes()
+    elif look_up == 'r':
+        reversals()
+    elif look_up == 'su':
+        submissions()
     elif look_up.lower() == 'c':
         create_new_data()
     elif look_up.lower() == 'q':
